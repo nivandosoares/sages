f5e92ac8056b34df457e58d4bde9c1db34874cf1
Nivando Soares <sidvandoni@gmail.com>
2024-01-05 18:02:39 -0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -20,39 +20,59 @@ const {
 } = require("../controllers/event_controller");
 
 const { login, createAdminUsers } = require("../controllers/user_controller");
+
+// Middleware for handling file uploads
 router.use(fileUpload());
 
-// Rota para a página inicial
+// Route for the home page
 router.get("/", getEvents);
+
+// Route for getting all events
 router.get("/events-all", getAllEvents);
+
+// Routes for getting approved, pending, and rejected events
 router.get("/aproved-events", getAprovedEvents);
 router.get("/pending-events", getPendingEvents);
 router.get("/rejected-events", getRejectedEvents);
 
-// Rota para o formulário de agendamento
+// Route for the event scheduling form
 router.get("/agendar", getForm);
+
+// Route for the sudo form
 router.get("/sudo", getSudoForm);
+
+// Route for rendering an event by id
 router.get("/status/:id", renderEvent);
 
+// Route for getting an event by id
 router.get("/event/:id", getEvent);
 
+// Route for creating admin users (should be run once)
 router.post("/first-run", createAdminUsers);
 
+// Routes for success and error pages
 router.get("/success", (req, res) => {
   res.render("success");
 });
-
 router.get("/error", (req, res) => {
   res.render("error");
 });
 
-// Rota para lidar com a submissão do formulário
+// Route for handling the form submission
 router.post("/agendar", createEvent);
+
+// Route for handling the sudo form submission
 router.post("/sudo", sudoCreateEvent);
+
+// Route for the login page
 router.get("/login", (req, res) => {
   res.render("login");
 });
 
+// Route for handling login
 router.post("/login", login);
+
+// Route for updating event status
 router.put("/event/:id", updateEventStatus);
+
 module.exports = router;
