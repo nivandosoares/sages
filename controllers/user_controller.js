6611c1425e7a9f486f6c393001bd36d46b9539ae
Nivando Soares <sidvandoni@gmail.com>
2024-01-05 17:52:41 -0300

    Update user_controller.js

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,9 +1,61 @@
+const fs = require("fs");
+const path = require("path");  // Don't forget to include the 'path' module
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const moment = require("moment");
 const User = require("../models/user_model");
-const Event = require("../models/event_model"); // Import the Event model
+const Event = require("../models/event_model");
+
 const { isAdmin } = require("./event_controller");
+
+const createAdminUsers = async (req, res) => {
+  try {
+    // Read the JSON file
+    const filePath = path.join(__dirname, "mail_list.json");
+    const jsonData = fs.readFileSync(filePath, "utf8");
+    const data = JSON.parse(jsonData);
+
+    // Iterate over institutes and courses to insert users
+    for (const institute of data.institutes) {
+      for (const course of institute.courses) {
+        const username = course.coordinator.toLowerCase().replace(" ", ""); // Extract the username as specified
+        const email = course.coordinatorEmail;
+        const password = `${course.coordinator.split(" ")[0].toLowerCase()}.coordenacao`; // Generate the password
+
+        try {
+          // Create the user in the database
+          const user = await User.create({
+            username,
+            role: "admin",
+            email,
+            password: bcrypt.hashSync(password, 10),
+          });
+
+          // Optionally, you can generate a token here and do something with it
+          const token = jwt.sign(
+            { id: user._id, role: user.role },
+            process.env.JWT_SECRET
+          );
+
+          // Output success message or do further processing if needed
+          console.log(`User created: ${user.username}`);
+        } catch (error) {
+          // If it's a duplicate key error, log a message and continue with the next iteration
+          if (error.code === 11000) {
+            console.warn(`Duplicate key error for email: ${email}. Skipping...`);
+            continue;
+          }
+
+          // If it's a different error, log the error
+          console.error("Error inserting user:", error);
+        }
+      }
+    }
+  } catch (error) {
+    console.error("Error inserting users from JSON:", error);
+  }
+};
+
+
 
 const signin = async (req, res) => {
   try {
@@ -70,4 +122,5 @@ module.exports = {
   login,
   signin,
   fetchUser,
+  createAdminUsers,
 };
